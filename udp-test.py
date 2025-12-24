197c2522a25c2057f8319dc0d1c6eacd2d3fbc78
patterniha <71074308+patterniha@users.noreply.github.com>
2025-12-24 10:57:23 +0330

    Update udp-test.py

--- udp-test.py
+++ udp-test.py
@@ -2,9 +2,10 @@ import socket
 import time
 
 
-for j in range(۱000):
+for j in range(1000):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.sendto(b"123", ("2.2.2.2", 443))
     print("sent", j+1)
     time.sleep(.001)
+
 
